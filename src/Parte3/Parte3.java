2ec24637bdf4384b4f12a4d2b3348b1adaee1a35
Cerozob <55302468+Cerozob@users.noreply.github.com>
2020-11-26 01:23:35 -0500

    parte 3

--- src/Parte3/Parte3.java
+++ src/Parte3/Parte3.java
@@ -9,63 +9,92 @@ import java.util.Stack;
 public class Parte3 {
 
 	public static void main(String[] args) {
+		String  path = args[0]; // ruta al archivo ingresa por argumentos del programa.
 		int[][] matrizAdyacencia = null;
 		try {
-			matrizAdyacencia = cargarMatriz(Integer.parseInt(args[0]));
+			matrizAdyacencia = cargarMatriz(path);
 		}
-		catch(FileNotFoundException e) {System.out.println("archivo /data/distances"+args[0]+".txt no encontrado");} catch (NumberFormatException e) {
+		catch(FileNotFoundException e) {System.out.println("archivo "+args[0]+" no encontrado");} catch (NumberFormatException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		if(matrizAdyacencia!=null) {
-			System.out.println(determinarCicloEnGrafoMatriz(matrizAdyacencia));}
+
+			for(Integer i :ordenTopologico(matrizAdyacencia))
+			{
+				System.out.println(i+1); // imprime el orden topológico del grafo, si imprime un repetido es un ciclo.
+			}
+		}
 		else {
-			System.out.println("No se cargó la matriz correctamente");}
+			System.out.println("No se cargó la matriz correctamente");
+		}
 	}
 
-	private static int[][] cargarMatriz(Integer i) throws IOException,FileNotFoundException {
+	private static Stack<Integer> ordenTopologico(int[][] matrizAdyacencia) {
+		Stack<Integer> ordenTopologico = new Stack<>(); //orden topológico del grafo
+		boolean[] marcados = new boolean[matrizAdyacencia.length]; // Los verices por los que yá pasé
+		boolean[] enEjecucion = new boolean[matrizAdyacencia.length]; // Los vértices que tienen un llamado recursivo en curso
+		boolean ciclo = false; //solo se usa para imprimir "hay ciclo" u "orden topológico" como título según sea el caso
+		for(int i =0;i<matrizAdyacencia.length;i++)
+		{
+			if(!marcados[i]) //si no he pasado por ahí
+			{
+				try {
+					dfs(marcados,ordenTopologico,matrizAdyacencia,i,enEjecucion);//Haga DFS sobre el vértice
+				} catch (Exception e) { //Excepción si hay un ciclo
+					System.out.println(e.getMessage());
+					ciclo=true;
+					break;
+				} 
+			}
+		}
+		if (!ciclo)
+		System.out.println("El orden topológico es ");
+		return ordenTopologico;
+	}
 
+	private static void dfs(boolean[] marcados,Stack<Integer> ordenTopologico,int[][] matrizAdyacencia, int i,boolean[] ejecucion) throws Exception {
 
-		BufferedReader br = new BufferedReader(new FileReader("/data/distances"+i+".txt"));
+		marcados[i]=true; //marcar que ya pasé por yo mismo
+		ejecucion[i]=true; //estoy haciendo DFS en este momento
+		for(int j =0;j<matrizAdyacencia[i].length;j++) //para cada adyacente
+		{
+			if(matrizAdyacencia[i][j] != -1 && i!=j)
+			{
+				if(marcados[j] && ejecucion[j])//mi adyacente es un vertice sobre el que ya llamé dfs y no ha terminado -> hallé un ciclo
+				{
+					ordenTopologico.push(j);
+					ordenTopologico.push(i);//pongo el repetido en la pila por convención, facilita ver el recorrido del ciclo.
+					throw new Exception("Se encontró un ciclo en el grafo"); // termino la ejecución puesto que encontré un ciclo
+				}
+				else if (!marcados[j])
+				{
+					dfs(marcados,ordenTopologico,matrizAdyacencia,j,ejecucion);
+				}
+			}
+		}
+		ejecucion[i]=false;//termine DFS
+		ordenTopologico.push(i);
+	}
+
+	private static int[][] cargarMatriz(String path) throws IOException,FileNotFoundException {
+
+		BufferedReader br = new BufferedReader(new FileReader(path));
+		br.mark(1000000); //el tamaño máximo de la matriz es de 500000
+		int i = br.readLine().split("\t").length;//sacar el tamaño a partir de la primera línea
+		br.reset(); // reiniciar el reader para cargar la matriz completa
 		String linea;
 		int[][] matriz = new int[i][i];
-		for(int j=0;(linea = br.readLine())!=null;j++)
+		for(int j=0;(linea = br.readLine())!=null;j++) //el índice j es necesario para cargar la matriz
 		{
-			String[] values = linea.split(" ");
+			String[] values = linea.split("\t");
 			for(int k=0;k<i;k++)
 			{
 				matriz[j][k]=Integer.parseInt(values[k]);
 			}
 		}
 		br.close();
-
 		return matriz;
 	}
-	
-	//TODO tener en cuenta que el grafo es dirigido
-	private static boolean determinarCicloEnGrafoMatriz(int[][] matriz) {
-		
-		boolean[] marcados = new boolean[matriz.length];
-		
-		Stack<Integer> pila = new Stack<>();
-		for(int i = 0;pila.size()<matriz.length;)
-		{		
-			for(int j = 0;j<matriz.length;j++)
-			{
-				if(matriz[i][j]!=-1 && !marcados[i])
-				{
-					marcados[i]=true;
-					pila.push(i);
-					i=j;
-				}
-				else if(matriz[i][j]>-1 && marcados[j])
-				{
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-	
 }
